138299eeeab0eccd99580f8391655a4b5bc35f89
akanksha4022 <akanksha70010@gmail.com>
2025-09-05 00:37:27 +0530

    added add to cart functionality

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,34 +1,5 @@
-const products = [{
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating:{
-        stars : 4.5,
-        count: 87
-    },
-    price: 1090
-
-},
-{
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating:{
-        stars : 4,
-        count: 127
-    },
-    price: 2095
-},{
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating:{
-        stars : 4.5,
-        count: 56
-    },
-    price: 799
-}
-];
-
 productHtml = '';
-
+//iterate and add each product and generate html
 products.forEach((item)=>{
     const html = `
         <div class="product-container">
@@ -50,7 +21,7 @@ products.forEach((item)=>{
           </div>
 
           <div class="product-price">
-            $${(item.price/100).toFixed(2)}
+            $${(item.priceCents/100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -75,13 +46,49 @@ products.forEach((item)=>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id = '${item.id}'>
             Add to Cart
           </button>
         </div>
     `
-    productHtml += html;
+    productHtml += html;//attaching each product html to show on webpage
    
 });
 
+//showing on webpage by using class name of the div in amazon.html
 document.querySelector(".js-product-grid").innerHTML = productHtml;
+
+//after clicking button product sjould be added to cart
+//we have to push the product along with the name and quantity of product
+//to add product we have to use an html attribute called data attribute to know what product are we clicking
+//button.dataset give us all the data attribute attached to the button
+//also data-'product-name' get camel cased in javascript as productName
+
+//1. checks if product is in the cart
+//2. if in the cart increase the quantity 
+//3. if not push in the cart
+
+
+document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
+  button.addEventListener("click",()=>{
+  const productId = button.dataset.productId;
+  let matchingItem;
+  cart.forEach((product)=>{
+    if(productId === product.productId){
+      matchingItem = product;      
+    };    
+  })  
+  if(matchingItem){
+    matchingItem.quantity+=1;
+  }else{
+    cart.push({
+      productId: productId,
+      quantity: 1
+      })
+  }
+  
+
+  console.log(cart);
+    
+  })
+})
