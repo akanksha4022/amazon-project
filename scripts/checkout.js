16ec6d7344aed53668b40e5dd12fb8e57a99422f
akanksha4022 <akanksha70010@gmail.com>
2025-10-02 13:31:42 +0530

    addd backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,8 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 //import '../data/cart-oop.js';
 import '../data/cart-class.js';
 
+import './backend-practice.js';
+
 
 renderCheckoutHeader();
 renderOrderSummary();
